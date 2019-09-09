19f5e30674c38af3b530656d19bac9818f274374
Sophie Rodriguez <svr27@miami.edu>
2019-09-09 13:54:05 -0400

    Changing Color Variables Example
    Class Monday - 09/09/19

---
+++ Class examples/090919_VariableChanges/sketch.js
@@ -0,0 +1,78 @@
+var pointX = 0;
+var pointY= 0;
+  // for nose
+
+var Xeye = 150;
+var Yeye = 150;
+  // x and y coordinates for eyes
+
+
+var FaceColor = "blue";
+// changing blue to the difference color
+
+
+function setup() {
+  // put setup code here
+createCanvas(500,500);
+
+
+  // choosing point as the center of my screen
+  pointX = width/2;
+  pointY= width/2;
+
+  // this establishes this will always be the center point.
+
+  console.log("pointX"+pointY+"pointY"+pointY);
+
+}
+
+function draw() {
+  // put drawing code here
+
+    Xeye = mouseX;
+    Yeye = mouseY;
+
+
+
+  background(FaceColor);
+
+  strokeWeight(5);
+
+//change througout the session you are going to ned a variable in orderto change color.
+
+
+  fill(255, 255, 255);
+  ellipse(Xeye,Yeye,100,100);//left eye
+  ellipse(Xeye+185,Yeye,100,100);//right eye
+
+
+  fill(0,0,0);
+  ellipse(Xeye,Yeye+11,60,60);// left pupil
+  ellipse(Xeye+180,Yeye+11,60,60);// right pupil
+
+  fill(255,255,255);
+  ellipse(Xeye-20,Yeye,20,20);
+  ellipse(Xeye+160,Yeye,20,20);
+
+
+  arc(pointX-10,pointY+50,200,200,0,PI);
+
+
+}// end of draw
+
+
+function mousePressed(){
+  FaceColor = "pink";
+
+}
+
+function mouseReleased(){
+  FaceColor = "blue";
+
+
+}
+
+function keyPressed(){
+  FaceColor = "white";
+
+}
