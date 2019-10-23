d4a9b8ae704fc505201a9ffdf80c28ae5ca99e0b
Sophie Rodriguez <svr27@miami.edu>
2019-10-23 12:31:35 -0400

    ReadMe File for Midterm

--- Class examples/10.09.19_Midterm/sketch.js
+++ Class examples/10.09.19_Midterm/sketch.js
@@ -18,36 +18,37 @@ var BottomArray = [];
 
 //Opening slide
 var OpenHitX = [0];
-var OpenHitY = [100];
-var OpenSize = 700;
+var OpenHitY = [0];
+var OpenSizeW = 1920;
+var OpenSizeH = 1080;
 
 var OpenWindow = false;
 
 // top buttons
-var hitX = [200,447];
-var hitY = [195,195];
+var hitX = [600,1200];
+var hitY = [400,400];
 
 // bottom buttons
-var hitX2 = [200,447];
-var hitY2 = [410,410];
+var hitX2 = [600,1200];
+var hitY2 = [800,800];
 
-var hitSize = 50;
+var hitSize = 150;
 
 // Dress me Button
 var dressMe = false;
-var hitX3 = [500];
-var hitY3 = [405];
+var hitX3 = [1350];
+var hitY3 = [800];
 
-var DressMeSizeW = 170;
-var DressMeSizeH = 60;
+var DressMeSizeW = 390;
+var DressMeSizeH = 150;
 
 // Browse button
 var browse = false;
-var hitX4 = [47];
-var hitY4 = [405];
+var hitX4 = [210];
+var hitY4 = [800];
 
-var BrowseButtonSizeW = 150;
-var BrowseButtonSizeH = 60;
+var BrowseButtonSizeW = 390;
+var BrowseButtonSizeH = 150;
 
 // Browse button timer
 var interval = 1000;
@@ -97,66 +98,50 @@ function preload(){
 
 function setup() {
   // put setup code here
-  createCanvas(700,530)
+  createCanvas(1960,1080)
   textFont(font);
 
 
 }// end of SETUP
 
-function circleColor(xpos, ypos, r,g,b, cSize){
-// working with shapes. Make sure that the name doesnt match with a variable/other function.
-// make it easier to draw a bunch of circles onto the screen.
-  fill(r,g,b);
-  ellipse(xpos, ypos, cSize, cSize);
-}
+
 
 function draw() {
 
     // opening Window
-    background(255);
+    background("pink");
 
+    strokeWeight(350);
+    stroke(218,82,144);
+    //noFill();
+    fill(218,82,144)
 
-
+    rect(OpenHitX,OpenHitY,OpenSizeW,OpenSizeH);
 
     noStroke();
-    rect(0,0,700,530);
-    for(var i = 0; i < 10000; i++){
-   circleColor(random(width), random(height), random(255), random(255), random(255), random(0,1));
-   stroke(0);
-   strokeWeight(1);
-    // cant slow it down with frameRate because the entire interaction slows down. It affects everything. MousePressed etc\
-    // use millus to slow down the framerate
-
-    }// end of FOR loop
-
-    stroke(0);
     strokeWeight(5);
-    textSize(50);
+    textSize(150);
     fill("white");
-    text("Welcome",100,130);
-    text("to the",100,180);
+    text("Welcome to the",200,250);
 
-    image(cluelessLogo,100,190,cluelessLogo.width,cluelessLogo.height);
-    fill("pink");
-    text("Closet...",100,370);
+    image(cluelessLogo,200,270,cluelessLogo.width*2.5,cluelessLogo.height*2.5);
+    fill("white");
+    text("Closet...",200,750);
 
-    fill("light pink");
-    textSize(30);
-    text("Click anywhere to start",100,400);
+    fill(144,238,144);
+    textSize(75);
+    text("Click anywhere to start!",460,940);
 
-    strokeWeight(100);
-    stroke(207, 185, 151);
-    noFill();
-    rect(0,0,700,530);
+
 
 
 if(OpenWindow == true){
     //var startTimer = true;}
 
-    image(leopardPng,0,0,leopardPng.width,leopardPng.height);
+    image(leopardPng,-20,-20,leopardPng.width*1.75,leopardPng.height*1.75);
     noStroke();
     fill(211,211,211);
-    rect(0,0,700,40);
+    //rect(0,0,700,40);
   //  image(cluelessLogo,250,-5,cluelessLogo.width/2,cluelessLogo.height/2);
   //  image(cluelessLogo,250,460,cluelessLogo.width/2,cluelessLogo.height/2);
 
@@ -164,13 +149,13 @@ if(OpenWindow == true){
     stroke("grey");
     strokeWeight(10);
     fill("white");
-    rect(200, 70, 297, 390);
+    rect(750, 80, 450, 860);
 
     console.log("currentTop" + currentTop);
     console.log("currentBottom" + currentBottom);
 
-    image(TopArray[currentTop],250,75,TopArray[currentTop].width/4,TopArray[currentTop].height/4);
-    image(BottomArray[currentBottom],250,250,BottomArray[currentBottom].width/4,BottomArray[currentBottom].height/4);
+    image(TopArray[currentTop],750,80,TopArray[currentTop].width/1.75,TopArray[currentTop].height/1.75);
+    image(BottomArray[currentBottom],750,500,BottomArray[currentBottom].width/1.75,BottomArray[currentBottom].height/1.75);
 
     strokeWeight(1);
     stroke(0);
@@ -184,132 +169,136 @@ if(OpenWindow == true){
     rect(hitX3[0],hitY3[0],DressMeSizeW,DressMeSizeH);
     rect(hitX4[0],hitY4[0],BrowseButtonSizeW,BrowseButtonSizeH);
 
-    textSize(30);
+    textSize(70);
     fill(0);
-    text("Dress Me", 510, 445);
-    text("Browse", 60, 445);
+    text("Dress Me", 1365, 900);
+    text("Browse", 270, 900);
 
     if(dressMe == true){
       if(currentTop == 0 && currentBottom == 2) {
          console.log("Outfit1");
          fill(192, 255, 0);
          strokeWeight(10);
-         rect(150, 200, 400, 100);
-         textSize(40);
+         rect(475, 400, 1000, 200);
+         textSize(95);
          noStroke();
          fill("black");
-         text("Match!", 270,250);
-         textSize(20);
-         text("Click a blue square to restart.", 180,270);
+         text("Match!", 800,510);
+         textSize(50);
+         text("Click a blue square to restart.", 550,560);
 
        }else if (currentTop == 0 && currentBottom == 0) {
          console.log("Outfit2");
          fill(192, 255, 0);
          strokeWeight(10);
-         rect(150, 200, 400, 100);
-         textSize(40);
+         rect(475, 400, 1000, 200);
+         textSize(95);
          noStroke();
          fill("black");
-         text("Match!", 270,250);
-         textSize(20);
-         text("Click a blue square to restart.", 180,270);
+         text("Match!", 800,510);
+         textSize(50);
+         text("Click a blue square to restart.", 550,560);
+
 
        }else if (currentTop == 1 && currentBottom == 3) {
          console.log("Outfit3");
          fill(192, 255, 0);
          strokeWeight(10);
-         rect(150, 200, 400, 100);
-         textSize(40);
+         rect(475, 400, 1000, 200);
+         textSize(95);
          noStroke();
          fill("black");
-         text("Match!", 270,250);
-         textSize(20);
-         text("Click a blue square to restart.", 180,270);
+         text("Match!", 800,510);
+         textSize(50);
+         text("Click a blue square to restart.", 550,560);
+
 
        }else if (currentTop == 2 && currentBottom == 0) {
         console.log("Outfit4");
         fill(192, 255, 0);
         strokeWeight(10);
-        rect(150, 200, 400, 100);
-        textSize(40);
+        rect(475, 400, 1000, 200);
+        textSize(95);
         noStroke();
         fill("black");
-        text("Match!", 270,250);
-        textSize(20);
-        text("Click a blue square to restart.", 180,270);
+        text("Match!", 800,510);
+        textSize(50);
+        text("Click a blue square to restart.", 550,560);
+
 
         }else if (currentTop == 2 && currentBottom == 1) {
        console.log("Outfit5");
        fill(192, 255, 0);
        strokeWeight(10);
-       rect(150, 200, 400, 100);
-       textSize(40);
+       rect(475, 400, 1000, 200);
+       textSize(95);
        noStroke();
        fill("black");
-       text("Match!", 270,250);
-       textSize(20);
-       text("Click a blue square to restart.", 180,270);
+       text("Match!", 800,510);
+       textSize(50);
+       text("Click a blue square to restart.", 550,560);
 
      }else if (currentTop == 3 && currentBottom == 4) {
       console.log("Outfit5");
       fill(192, 255, 0);
       strokeWeight(10);
-      rect(150, 200, 400, 100);
-      textSize(40);
+      rect(475, 400, 1000, 200);
+      textSize(95);
       noStroke();
       fill("black");
-      text("Match!", 270,250);
-      textSize(20);
-      text("Click a blue square to restart.", 180,270);
+      text("Match!", 800,510);
+      textSize(50);
+      text("Click a blue square to restart.", 550,560);
+
 
     }else if (currentTop == 2 && currentBottom == 6) {
      console.log("Outfit5");
      fill(192, 255, 0);
      strokeWeight(10);
-     rect(150, 200, 400, 100);
-     textSize(40);
+     rect(475, 400, 1000, 200);
+     textSize(95);
      noStroke();
      fill("black");
-     text("Match!", 270,250);
-     textSize(20);
-     text("Click a blue square to restart.", 180,270);
+     text("Match!", 800,510);
+     textSize(50);
+     text("Click a blue square to restart.", 550,560);
 
 
    }else if (currentTop == 5 && currentBottom == 7) {
       console.log("Outfit5");
       fill(192, 255, 0);
       strokeWeight(10);
-      rect(150, 200, 400, 100);
-      textSize(40);
+      rect(475, 400, 1000, 200);
+      textSize(95);
       noStroke();
       fill("black");
-      text("Match!", 270,250);
-      textSize(20);
-      ttext("Click a blue square to restart.", 180,270);
+      text("Match!", 800,510);
+      textSize(50);
+      text("Click a blue square to restart.", 550,560);
 
     }else if (currentTop == 4 && currentBottom == 6) {
        console.log("Outfit5");
        fill(192, 255, 0);
        strokeWeight(10);
-       rect(150, 200, 400, 100);
-       textSize(40);
+       rect(475, 400, 1000, 200);
+       textSize(95);
        noStroke();
        fill("black");
-       text("Match!", 270,250);
-       textSize(20);
-       text("Click a blue square to restart.", 180,270);
+       text("Match!", 800,510);
+       textSize(50);
+       text("Click a blue square to restart.", 550,560);
 
      }else if (currentTop == 5 && currentBottom == 6) {
         console.log("Outfit5");
         fill(192, 255, 0);
         strokeWeight(10);
-        rect(150, 200, 400, 100);
-        textSize(40);
+        rect(475, 400, 1000, 200);
+        textSize(95);
         noStroke();
         fill("black");
-        text("Match!", 270,250);
-        textSize(20);
-        text("Click a blue square to restart.", 180,270);
+        text("Match!", 800,510);
+        textSize(50);
+        text("Click a blue square to restart.", 550,560);
 
 
 
@@ -317,13 +306,13 @@ if(OpenWindow == true){
 
       fill("red");
       strokeWeight(10);
-      rect(150, 200, 400, 100);
-      textSize(40);
+      rect(475, 400, 1000, 200);
+      textSize(95);
       noStroke();
       fill("white");
-      text("MisMatch!", 240,250);
-      textSize(20);
-      text("Click a blue square to restart.", 180,270);
+      text("MisMatch!", 700,510);
+      textSize(50);
+      text("Click a blue square to restart.", 550,560);
 
     }
   }// end of IF
@@ -340,24 +329,21 @@ if(OpenWindow == true){
 
     if(millisCounter == 10){
       browse = false;
-      millisCounter = 0;
-
-    }
+      millisCounter = 0;}
 
 }// end of browse
 
 }// end of If Open Window
-}// end of DRAW
 
-//
+}
+
+
+
 function mousePressed(){
-
-  if(mouseX > OpenHitX[0] && mouseX < OpenHitX[0] + OpenSize && mouseY > OpenHitY[0] && mouseY < OpenHitY[0] + OpenSize){
+  if(mouseX > OpenHitX[0] && mouseX < OpenHitX[0] + OpenSizeW && mouseY > OpenHitY[0] && mouseY < OpenHitY[0] + OpenSizeH){
   console.log("Click to Closet");
   OpenWindow = true;
   }
-
-
 
 
   if(mouseX > hitX[0] && mouseX < hitX[0] + hitSize && mouseY > hitY[0] && mouseY < hitY[0] + hitSize){
@@ -385,9 +371,9 @@ function mousePressed(){
         currentBottom = 7;
       }
     dressMe = false;
-}// end of IF 3
+  }// end of IF 3
 
-if(mouseX > hitX2[1] && mouseX < hitX2[1] + hitSize && mouseY > hitY2[1] && mouseY < hitY2[1] + hitSize){
+  if(mouseX > hitX2[1] && mouseX < hitX2[1] + hitSize && mouseY > hitY2[1] && mouseY < hitY2[1] + hitSize){
     console.log("Button 4");
     currentBottom = currentBottom + 1;
     if(currentBottom == 8){
@@ -405,11 +391,9 @@ if(mouseX > hitX2[1] && mouseX < hitX2[1] + hitSize && mouseY > hitY2[1] && mous
 
   if(mouseX > hitX4[0] && mouseX < hitX4[0] + BrowseButtonSizeW && mouseY > hitY4[0] && mouseY < hitY4[0] + BrowseButtonSizeH){
         console.log("BrowseButton");
-
+        dressMe = false;
         browse = true;
         prevMillis = millis();
 
-
-      }// end of IF 5
-
-}// end of MousePressed
+    }// end of IF 5
+  }
