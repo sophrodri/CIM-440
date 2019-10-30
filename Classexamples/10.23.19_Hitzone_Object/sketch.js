e7c0abf9a9b0393ac18c5e55bdda3e97c543d162
Sophie Rodriguez <svr27@miami.edu>
2019-10-30 13:40:53 -0400

    class examples

--- Classexamples/10.23.19_Hitzone_Object/sketch.js
+++ Classexamples/10.23.19_Hitzone_Object/sketch.js
@@ -1,13 +1,17 @@
 
 var button1;
-
+var button2;
 
 function setup() {
   // put setup code here
 createCanvas(400,400);
-button1 = new hitzoneObject(100,200,50,"red");
-button2 = new hitzoneObject(200,200,50,"green");
+button1 = new hitzoneObject(100,200,50,"red", "Stop button", loadImage("assets/stop.png"));
+button2 = new hitzoneObject(200,200,50,"green", "Play button", loadImage("assets/play.png"));
 }
+
+
+
+
 
 function draw() {
   // put drawing code here
@@ -18,11 +22,27 @@ button1.check();
 console.log("button1 overlay" + button1.overlay);
 button2.display();
 button2.check();
-}
+}// end of Draw
+
+
+
+
+function mousePressed()  {
+  if(button1.overlay == true){
+    console.log(button1.label);
+  }
+
+  if(button2.overlay == true){
+    console.log(button2.label);
+  }
+
+
+}// end of MousePressed
+
 
 
 class hitzoneObject{
-  constructor(tempX, tempY, tempSize, tempColor){
+  constructor(tempX, tempY, tempSize, tempColor, tempLabel, tempImage){
     // building blocks for the individual objects
     // similar to passing parameters into a created Function
     // grab the parameters and pass it to variables that are representative to the object we are creating.
@@ -33,14 +53,34 @@ class hitzoneObject{
     this.boxSize = tempSize;
     this.boxColor = tempColor;
     this.overlay = false;
+    this.label = tempLabel;
+    this.image = tempImage;
   }//end of constructor
+
+
+
+
 
 // the function of a specific object!
 display(){
 fill(this.boxColor);
-rect(this.x, this.y, this.boxSize, this.boxSize);
+//rect(this.x, this.y, this.boxSize, this.boxSize); We are building on this object, Replacing rects with image
+image(this.image, this.x, this.y, this.boxSize, this.boxSize);
+text(this.label, this.x, this.y-20);
 
+// if stays that says if the overlay is true, draw the graphic element that shows you are hovering over the button1
+
+if(this.overlay == true){
+  // overlay is true when the mouse is over the button
+  fill(127,127)// 127 is gray, 127 is opacity
+  rect(this.x, this.y, this.boxSize, this.boxSize);
+}
 }// end of Display
+
+
+
+
+
 
 check(){
 // check mouse versus XY and location of button2
---
+++ Classexamples/10.30.19_Intro_to_DOM/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+  // msot dom functions go on the setup? to stop looping continuously
+  cnv.id("mycanvas";)
+
+  var container0 = createDiv();// designates a section on the page 
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+}
