f945572f42304010b9b230e50f7a2540ca47a2c4
Sophie Rodriguez <svr27@miami.edu>
2019-12-06 12:10:24 -0500

    Problem with createButton
    rearragned the buttons and added program logo

--- Classexamples/FINAL/sketch.js
+++ Classexamples/FINAL/sketch.js
@@ -1,12 +1,20 @@
 let capture;
 var cnv;
 
-
-var whiteButton, rainbowButton;
+var whiteButton, rainbowButton, noFilterButton, sunsetButton, greenButton, blueButton, pinkButton, cameraButton;
 
 var currentFilter = 0;
 
 var currentColor;
+
+var titlefont;
+
+
+
+
+
+
+
 
 function preload(){
 white = loadImage("Images/whitefilter.png")
@@ -15,80 +23,133 @@ peach = loadImage("Images/SunsetGradient.png")
 blue = loadImage("Images/blue.png")
 green = loadImage("Images/GreenGradient.png")
 pink = loadImage("Images/pink.png")
-
+titlefont = loadFont("Fonts/Canterbury.ttf")
 }
 
+
+noFilterButton = createButton("No Filter");
+noFilterButton.position(300,475);
+noFilterButton.mousePressed(function(){
+  currentFilter = 0; // assign image to number
+});
+
+whiteButton = createButton("White");
+whiteButton.position(300,500);
+whiteButton.mousePressed(function(){
+  currentFilter = 1; // assign image to number
+});
+
+rainbowButton = createButton("Rainbow");
+rainbowButton.position(370,500);
+rainbowButton.mousePressed(function(){
+  currentFilter = 2; // assign image to number
+});
+
+sunsetButton = createButton("Sunset");
+sunsetButton.position(300,550);
+sunsetButton.mousePressed(function(){
+  currentFilter = 3; // assign image to number
+});
+
+blueButton = createButton("Blue");
+blueButton.position(470,500);
+blueButton.mousePressed(function(){
+  currentFilter = 4; // assign image to number
+});
+
+greenButton = createButton("Green");
+greenButton.position(470,550);
+greenButton.mousePressed(function(){
+  currentFilter = 5; // assign image to number
+});
+
+pinkButton = createButton("Pink");
+pinkButton.position(400,550);
+pinkButton.mousePressed(function(){
+  currentFilter = 6; // assign image to number
+});
+
+cameraButton = createButton("Click here to take a picture!")
+cameraButton.position(300,600);
+cameraButton.mousePressed(function(){
+  saveCanvas(cnv, 'myCanvas', 'jpg');
+});
+
 function setup() {
-  cnv = createCanvas(1900, 1000);//480, 480
+  cnv = createCanvas(1690, 1000);//480, 480
   capture = createCapture(VIDEO);
   capture.id = "vid";
   capture.hide();
 
+
   noFilterButton = createButton("No Filter");
-  noFilterButton.position(300,475);
+  noFilterButton.position(450,900);
   noFilterButton.mousePressed(function(){
     currentFilter = 0; // assign image to number
   });
 
-
   whiteButton = createButton("White");
-  whiteButton.position(300,500);
+  whiteButton.position(550,900);
   whiteButton.mousePressed(function(){
     currentFilter = 1; // assign image to number
   });
 
   rainbowButton = createButton("Rainbow");
-  rainbowButton.position(370,500);
+  rainbowButton.position(650,900);
   rainbowButton.mousePressed(function(){
     currentFilter = 2; // assign image to number
   });
 
   sunsetButton = createButton("Sunset");
-  sunsetButton.position(300,550);
+  sunsetButton.position(750,900);
   sunsetButton.mousePressed(function(){
     currentFilter = 3; // assign image to number
   });
 
   blueButton = createButton("Blue");
-  blueButton.position(470,500);
+  blueButton.position(850,900);
   blueButton.mousePressed(function(){
     currentFilter = 4; // assign image to number
   });
 
   greenButton = createButton("Green");
-  greenButton.position(470,550);
+  greenButton.position(950,900);
   greenButton.mousePressed(function(){
     currentFilter = 5; // assign image to number
   });
 
   pinkButton = createButton("Pink");
-  pinkButton.position(400,550);
+  pinkButton.position(1050,900);
   pinkButton.mousePressed(function(){
     currentFilter = 6; // assign image to number
   });
 
   cameraButton = createButton("Click here to take a picture!")
-  cameraButton.position(300,600);
+  cameraButton.position(1150,890);
+  cameraButton.style('color:magenta')
+  cameraButton.size(200,50);
   cameraButton.mousePressed(function(){
     saveCanvas(cnv, 'myCanvas', 'jpg');
   });
 
-  currentColor = white;
+  textFont(titlefont);
 
 }//end of setup
 
 
 function draw() {
 
+
 image(capture, 0, 0, width, width * capture.height / capture.width);
   //filter(INVERT);
 //image(FilterSelect[0], 0, 0, width*2, height*2);
 
-//
+currentColor = white;
+
+
 if(currentFilter >= 1){
   //draw image
   image(currentColor,0, 0, width, height);
-
 }
   // move this into ^
 
@@ -112,11 +173,19 @@ if(currentFilter==1){
 
 }else if (currentFilter==6){
   currentColor = pink;
-
-//}else if (currentFilter==-1,7){
-
 }
-}
+
+noStroke();
+textSize(100);
+fill("white");
+text("photosoph",75,900);
+
+noStroke();
+textSize(30);
+fill("white");
+text("Photobooth by SophRodri",100,950);
+
+}// end of draw
 
 image(currentColor,0,0, currentColor.width/4,currentColor.height/4);
 
